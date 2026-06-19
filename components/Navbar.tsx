c07214d4aa33bf8853128363aee80006ffa92859
Daniel Ugochukwu <slade8546@gmail.com>
2026-06-19 12:02:04 +0100

    Implement search functionality and update Hero and Navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,11 +29,11 @@ const Navbar = () => {
         <Link href={`/`} className="flex gap-0.5 items-center">
           <Image
             src={`/assets/logo.png`}
-            alt="Bookified"
+            alt="Bookara"
             width={42}
             height={26}
           />
-          <span className="logo-text">Bookified</span>
+          <span className="logo-text">Bookara</span>
         </Link>
 
         <nav className="w-fit flex gap-7.5 items-center">
